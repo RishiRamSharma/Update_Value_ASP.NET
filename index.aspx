8a85ac18777d91ac9e76d1cf60ee91f837472261
RishiRamSharma <rishiram86075@gmail.com>
2022-10-04 14:24:11 +0530

    Add project files.

---
+++ index.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="teacher_project.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Teacher&#39;s Details :-<br />
+            <br />
+            Id&nbsp;:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="TextId" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Name&nbsp;:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="TextName" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Subject&nbsp;:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;<asp:TextBox ID="TextSub" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Department&nbsp;:&nbsp;
+            <asp:TextBox ID="TextDep" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Show details :&nbsp;&nbsp;
+            <asp:ListBox ID="ShowList" runat="server" Height="96px" Width="231px" AutoPostBack="True" OnSelectedIndexChanged="ShowList_SelectedIndexChanged"></asp:ListBox>
+            <br />
+            <br />
+            <br />
+            <asp:Button ID="save_btn" runat="server" Text="Save" OnClick="save_btn_Click" />
+            <asp:Button ID="display_btn" runat="server" Text="Display" OnClick="display_btn_Click" />
+            <asp:Button ID="update_btn" runat="server" Text="Update" OnClick="update_btn_Click" />
+            <asp:Button ID="delete_btn" runat="server" Text="Delete" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ index.aspx.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace teacher_project
+{
+    public partial class index : System.Web.UI.Page
+    {
+        SqlConnection conn = new SqlConnection();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            conn.ConnectionString = ConfigurationManager.ConnectionStrings["techKey"].ConnectionString;
+            if(conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+        }
+
+        protected void save_btn_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "insert teacher_table values(@teachid,@teachname,@teachsub,@teachdept)";
+            cmd.Connection = conn;
+            cmd.Parameters.AddWithValue("@teachid", TextId.Text);
+            cmd.Parameters.AddWithValue("@teachname", TextName.Text);
+            cmd.Parameters.AddWithValue("@teachsub", TextSub.Text);
+            cmd.Parameters.AddWithValue("@teachdept", TextDep.Text);
+            cmd.ExecuteNonQuery();
+            cmd.Dispose();
+            clear_refactor();
+            display_refactor();
+
+        }
+
+        private void clear_refactor()
+        {
+            TextId.Text = string.Empty;
+            TextName.Text = string.Empty;
+            TextSub.Text = string.Empty;
+            TextDep.Text = string.Empty;
+            TextId.Focus();
+        }
+
+        protected void display_btn_Click(object sender, EventArgs e)
+        {
+            display_refactor();
+        }
+
+        private void display_refactor(){
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "select * from teacher_table";
+            cmd.Connection = conn;
+            SqlDataReader teachReader;
+            teachReader = cmd.ExecuteReader();
+            ShowList.DataTextField = "TeacherName";
+            ShowList.DataValueField = "TeacherID";
+            ShowList.DataSource = teachReader;
+            ShowList.DataBind();
+            teachReader.Close();
+            cmd.Dispose();
+        }
+
+        protected void ShowList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "select * from teacher_table where TeacherID = @teachid";
+            cmd.Connection = conn;
+            cmd.Parameters.AddWithValue("@teachid", ShowList.SelectedValue);
+            SqlDataReader teachReader = cmd.ExecuteReader();
+            if (teachReader.HasRows)
+            {
+                teachReader.Read();
+                TextId.Text = teachReader["TeacherID"].ToString();
+                TextName.Text = teachReader["TeacherName"].ToString();
+                TextSub.Text = teachReader["TeacherSubject"].ToString();
+                TextDep.Text = teachReader["TeacherDept"].ToString();
+
+            }
+            teachReader.Close();
+            cmd.Dispose();
+        }
+
+        protected void update_btn_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "update teacher_table set TeacherName=@teachname, TeacherSubject=@teachsub, TeacherDept=@teachdept where TeacherID=@teachid";
+            cmd.Connection = conn;
+            cmd.Parameters.AddWithValue("@teachid", TextId.Text);
+            cmd.Parameters.AddWithValue("@teachname", TextName.Text);
+            cmd.Parameters.AddWithValue("@teachsub", TextSub.Text);
+            cmd.Parameters.AddWithValue("@teachdept", TextDep.Text);
+            cmd.ExecuteNonQuery();
+            cmd.Dispose();
+            display_refactor();
+            clear_refactor();
+            
+        }
+    }
+}
